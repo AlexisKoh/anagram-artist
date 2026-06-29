791b1b0392bbed81e80f6eb7c096a511f6e02613
AlexisKoh <kohqiian113@gmail.com>
2026-06-29 10:14:41 +0800

    Update words.js

--- words.js
+++ words.js
@@ -1,7 +1,26 @@
-const WORDS = [
-  "ACDIN",
-  "LMOPU",
-  "EILNT",
-  "AENSV",
-  "IOPSU"
-];
+let WORDS = JSON.parse(localStorage.getItem("words")) || [];
+function saveWords() {
+  localStorage.setItem("words", JSON.stringify(WORDS));
+}
+function importFile(event) {
+  const file = event.target.files[0];
+  const reader = new FileReader();
+
+  reader.onload = function(e) {
+    const text = e.target.result;
+
+    const newWords = text
+      .toUpperCase()
+      .split("\n")
+      .map(w => w.trim())
+      .filter(w => /^[A-Z]{5}$/.test(w));
+
+    WORDS.push(...newWords);
+    WORDS = [...new Set(WORDS)];
+
+    saveWords();
+    alert("Imported " + newWords.length + " words!");
+  };
+
+  reader.readAsText(file);
+}
